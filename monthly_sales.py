52cd57f2be214b36cb6df916c18c6e50d6d1f4be
madelinenlee <madelinenlee@gmail.com>
2019-02-07 15:45:23 -0500

    adapted code from sales-reporting, chart-exercise

--- monthly_sales.py
+++ monthly_sales.py
@@ -10,10 +10,65 @@ import pandas as pd
 import operator
 import numpy as np
 import os
+from collections import OrderedDict
+import plotly as py
+import plotly.graph_objs as go
+
 
 user_input = '/Users/madeline/Desktop/SPRING_2019/OPIM_243/sales-reporting-exercise/data/sales-201710.csv'
 #input('please input the file pathname to load: ')
 
 sales_data = pd.read_csv(user_input)
+attributes = sales_data.columns.tolist()
+sales_data['date'] = pd.to_datetime(sales_data['date'])
+month = sales_data['date'][0].strftime('%B')
+year = sales_data['date'][0].strftime('%Y')
+
+print('SALES REPORT ('+ sales_data['date'][0].strftime('%B') +
+                     ' ' + sales_data['date'][0].strftime('%Y') +
+                     ')')
+print('TOTAL SALES : $', str('%0.2f'%sales_data['sales price'].sum()))
+
+products = sales_data['product'].unique().tolist()
+
+product_subtotals = {}
+
+for item in products:
+    product_subset = sales_data[sales_data['product'] == item]
+    product_total = product_subset['sales price'].sum()
+    product_subtotals[item] = product_total
+
+sorted_product_subtotals = sorted(product_subtotals.items(), key = operator.itemgetter(1))
+product_subtotals = dict(reversed(sorted_product_subtotals))
+
+print('TOP 3 SELLING PRODUCTS: ')
+
+count = 1
+for item in product_subtotals:
+    print(str(count) + '. ' + item + ' $' + str('%0.2f'%round(product_subtotals[item], 2)))
+    count = count + 1
+    if count > 3:
+        break
+
+x = []
+y = []
+
+for item in product_subtotals:
+    y.append(item)
+    x.append('$' + str(product_subtotals[item]))
+
+x = list(reversed(x))
+
+data = [go.Bar(
+            x=x,
+            y=y,
+            orientation='h'
+    )]
+layout = go.Layout(title='Top Selling Products (' + month + ' ' + year + ')',
+                   xaxis = dict(title='USD'),
+                   yaxis = dict(title='Product')
+                   )
+figure = go.Figure(data = data,layout=layout)
+py.offline.plot(figure, filename='horizontal-bar.html', auto_open = True)
 
 
