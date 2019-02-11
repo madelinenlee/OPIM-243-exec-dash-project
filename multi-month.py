249ba61a9709c59488db710be7523205191b826c
madelinenlee <madelinenlee@gmail.com>
2019-02-11 17:52:40 -0500

    begin interactive plotly graph

--- multi-month.py
+++ multi-month.py
@@ -32,6 +32,14 @@ while user_input != 'DONE':
     
     else:
         print('Error: input is not in right format (must have .csv as suffix!) Try again.')
+
+def validate_dataframe(data_frame, master_attributes):
+    attributes = data_frame.columns.tolist()
+    attributes = sorted(attributes)
+    
+    if attributes != master_attributes:
+        print('Error: csv formatted incorrectly - headers do not match')
+        
  
 def get_months(pathname_list):
     month_list = []
@@ -49,6 +57,7 @@ def create_master_dataframe(pathname_list):
         df_list.append(temp_dataframe)
     
     final_frame = pd.concat(df_list)
+    return(final_frame)
 
 def get_total_sales(data_frame):
     return(data_frame['sales price'].sum())
@@ -68,5 +77,65 @@ def create_product_sales_dict(data_frame):
     
     return(product_subtotals)
     
+
+#function adapted from previous project i completed on NFL historical data
 def create_line_graph(data_frame):
-    data_frame['date']
+    colors = ['#33CFA5','orange','#F06A6A','blue', 'violet',
+              'yellowgreen','aliceblue','lightgoldenrodyellow']
+    products = data_frame['products'].unique().tolist()
+    
+    data_list = []
+    
+    for i in range(0, len(products)):
+        temp_frame = data_frame[data_frame['product'] == products[i]]
+        
+        temp_scatter = go.Scatter(x=temp_frame['date'],
+                              y=temp_frame['sales price'],
+                              name= products[i],
+                              line=dict(color=colors[i])
+                                        )
+        data_list.append(temp_scatter)
+    
+    updatemenus = list([
+    dict(type="buttons",
+         active=-1,
+         buttons=list([
+            dict(label = 'QB',
+                 method = 'update',
+                 args = [{'visible': [True, False, False, False]},
+                         {'title': 'QB Pass Yards (Yards) vs Humidity (%)',
+                          'annotations': []}]
+                         ),
+            dict(label = 'WR',
+                 method = 'update',
+                 args = [{'visible': [False,True, False,False]},
+                         {'title': 'WR Receiving Yards (Yards) vs Humidity (%)',
+                          'annotations': []}]),
+            dict(label = 'K',
+                 method = 'update',
+                 args = [{'visible': [False, False,True, False]},
+                         {'title': 'K Field Goal Percentage (%) vs Humidity (%)',
+                          'annotations': []}]),
+            dict(label = 'RB',
+                 method = 'update',
+                 args = [{'visible': [False, False, False, True]},
+                         {'title': 'RB Rush Yards (Yards)',
+                          'annotations': []}]),
+            dict(label = 'All',
+                 method = 'update',
+                 args = [{'visible': [True, True, True, True]},
+                         {'title': 'Position Metrics vs Humidity (%)',
+                          'annotations': []}]),
+            dict(label = 'Reset',
+                 method = 'update',
+                 args = [{'visible': [True, True, True, True]},
+                         {'title': 'Position Metrics vs Humidity (%)',
+                          'annotations': []}])
+            ]),
+        )
+    ])
+        
+        
+        
+    
+    
